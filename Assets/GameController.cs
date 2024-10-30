130d916dae6e56f0be274a67050753efae8a3968
Nube-Lube <103972748+Nube-Lube@users.noreply.github.com>
2024-10-30 16:03:38 -0400

    test

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
 
-    public GameObject playerUnit;
+    public GameObject playerUnit, player1Hold, player2Hold;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +20,11 @@ public class GameController : MonoBehaviour
 
     public void tryPlace1(float x, float y)
     {
-        
+        Instantiate(player1Hold, player1Hold.transform);
     }
 
     public void tryPlace2(float x, float y)
     {
-
+        Instantiate(player2Hold, player2Hold.transform);
     }
 }
--- Assets/unitPlaceScript.cs
+++ Assets/unitPlaceScript.cs
@@ -6,6 +6,9 @@ public class unitPlaceScript : MonoBehaviour
 {
     public GameController gameController;
     public Vector2 playerMouse;
+    public CapsuleCollider capCollider;
+    public float movementSpeed;
+    public Rigidbody2D rb;
     public bool isPlayer1;
     // Start is called before the first frame update
     void Start()
@@ -16,14 +19,64 @@ public class unitPlaceScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isPlayer1 && Input.GetKeyDown(KeyCode.F))
+        rb.velocity *= 0.9f;
+        switch (isPlayer1)
         {
-            gameController.tryPlace1(playerMouse.x, playerMouse.y);
+            case true:
+                if (Input.GetKey(KeyCode.F))
+                {
+                    gameController.tryPlace1(playerMouse.x, playerMouse.y);
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    rb.AddForce(new Vector2(0, 1));
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    rb.AddForce(new Vector2(-1, 0));
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    rb.AddForce(new Vector2(0, -1));
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    rb.AddForce(new Vector2(1, 0));
+                }
+                break;
+
+            case false:
+                if (Input.GetKeyDown(KeyCode.Slash))
+                {
+                    gameController.tryPlace2(playerMouse.x, playerMouse.y);
+                }
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    rb.AddForce(new Vector2(0, 1));
+                }
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    rb.AddForce(new Vector2(-1, 0));
+                }
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    rb.AddForce(new Vector2(0, -1));
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    rb.AddForce(new Vector2(1, 0));
+                }
+                break;
         }
 
         if (!isPlayer1 && Input.GetKeyDown(KeyCode.Slash))
         {
             gameController.tryPlace2(playerMouse.x, playerMouse.y);
         }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("unit"))
+            Physics.IgnoreCollision(other.collider, capCollider);
     }
 }
